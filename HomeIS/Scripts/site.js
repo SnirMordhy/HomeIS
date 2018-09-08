d03fe8dd535e7564b9f2226ef3429cc0585c5278
Itai Hay <3392524+itaihay@users.noreply.github.com>
2018-09-08 12:05:33 +0000

    Added a modal with dynamic data loading

--- HomeIS/Scripts/site.js
+++ HomeIS/Scripts/site.js
@@ -10,10 +10,57 @@
     $.ajax({
         dataType: "json",
         url: "/Apartments/AllApartmentsJSON",
-        success: function (data) { updateApartmentList(data) }
+        success: function (data)
+        {
+            window.apartments = data;
+            updateApartmentList(data);
+        }
     });
 
 });
+
+function updateModalData(apartment) {
+
+    $('.carousel-indicators').empty();
+    $('.carousel-inner').empty();
+    $('.modal-caption').empty();
+
+    $.each(apartment.PhotoList, function (index, photo) {
+
+        $('.carousel-indicators').append('<li data-target="#apartment-photos-carousel" data-slide-to="' + index + '"></li>');
+        $('.carousel-inner').append('<div style="background-image:url(' + "\'" + photo + "\'" + ')" class="item ' + (index == 0 ? "active" : "") + '"><img style="display: none" src="' + photo + '"></div>');
+    });
+
+    $('.modal-caption').append(
+        '            <h4 class="thumbnail-caption-header">' + apartment.Location.City + ', <small>' + apartment.Location.Neighborhood + '</small></h4>'
+        + '            <div class="row">'
+        + '                <div class="col-md-6">'
+        + '                    <ul class="list-group">'
+        + '                        <li class="list-group-item">'
+        + '                            <span class="badge">' + apartment.Size + '</span>'
+        + '                            Sqr Meters'
+        + '                        </li>'
+        + '                        <li class="list-group-item">'
+        + '                            <span class="badge">' + apartment.NumberOfRooms + '</span>'
+        + '                            # Rooms'
+        + '                        </li>'
+        + '                        <li class="list-group-item">'
+        + '                            <span class="badge">' + (apartment.Balcony ? "Yes" : "No") + '</span>'
+        + '                            Balcony'
+        + '                        </li>'
+        + '                        <li class="list-group-item">'
+        + '                            <span class="badge">' + apartment.FloorNumber + '</span>'
+        + '                            Floor #'
+        + '                        </li>'
+        + '                    </ul>'
+        + '                </div>'
+        + '                <div class="col-md-6">'
+        + '                    <div style="min-height: 100%"><iframe width="100%" height="100%" src="https://maps.google.com/maps?width=100%&amp;height=100&amp;hl=en&amp;q=' + encodeURIComponent(apartment.Location.Address) + '+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div><br />'
+        + '                </div>'
+        + '            </div>'
+
+    );
+};
 
 function updateApartmentList(data)
 {
@@ -23,7 +70,7 @@ function updateApartmentList(data)
 
                 '<div class="col-sm-6 col-md-4">'
                 + '<div class="thumbnail">'
-                + '    <img src="' + apartment.PhotoList[0] + '" style="height: 200px; width: 100%; display: block;">'
+                + '    <a href="#"><img data-apartmentid=' + index + ' src = "' + apartment.PhotoList[0] + '" class= "apartment-image" style = "height: 200px; width: 100%; display: block;"></a> '
                 + '        <div class="caption ">'
                 + '            <h4 class="thumbnail-caption-header">' + apartment.Location.City + ', <small>' + apartment.Location.Neighborhood + '</small></h4>'
                 + '            <div class="row">'
@@ -58,5 +105,15 @@ function updateApartmentList(data)
 
             );
 
-        });
+    });
+
+
+
+    $('.apartment-image').click(function ()
+    {
+        var apartmentData = window.apartments[parseInt(this.dataset['apartmentid'])]
+        updateModalData(apartmentData)
+
+        $('#apartment-modal').modal('toggle')
+    });
 }
