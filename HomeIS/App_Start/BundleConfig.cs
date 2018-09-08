037db1b17a037005d1d13a8f02ac9fa41ac8b3c5
Itai Hay <3392524+itaihay@users.noreply.github.com>
2018-09-08 12:41:00 +0000

    Changing the apartment view

--- HomeIS/App_Start/BundleConfig.cs
+++ HomeIS/App_Start/BundleConfig.cs
@@ -11,6 +11,10 @@ namespace HomeIS
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js", "~/Scripts/site.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/sitewide").Include( "~/Scripts/site.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/apartment").Include("~/Scripts/apartment.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
