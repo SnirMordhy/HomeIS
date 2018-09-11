814f48bf8aadc5b4b0192a4265c8db3310b07f02
Sapir Nassimi <sapir.nassimi@gmail.com>
2018-09-11 13:29:12 +0300

    feature: 2 queries added. filtering by price, balcony and size.

--- HomeIS/Controllers/ApartmentsController.cs
+++ HomeIS/Controllers/ApartmentsController.cs
@@ -187,6 +187,27 @@ namespace HomeIS.Controllers
             var apartments = db.Apartments.ToList();
             return Json(apartments, JsonRequestBehavior.AllowGet);
         }
-        
+
+        public JsonResult SizeBalconyMinOrMaxPriceJSON(int Size, bool Balcony, int Price, bool IsMinimumPrice)
+        {
+            if (IsMinimumPrice)
+            {
+                return Json(db.Apartments.Include(t => t.Owner).Where(p => p.Balcony == Balcony && p.Size == Size && 
+                    p.PropertyValue >= Price).ToList(), JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(db.Apartments.Include(t => t.Owner).Where(p => p.Balcony == Balcony && p.Size == Size && 
+                    p.PropertyValue <= Price).ToList(), JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public JsonResult SizeBalconyPriceRangeJSON(int Size, bool Balcony, int MinimumPrice, int MaximumPrice)
+        {
+            var QuerySet = db.Apartments.Include(t => t.Owner).Where(p => p.Balcony == Balcony && p.Size == Size &&
+                    p.PropertyValue >= MinimumPrice && p.PropertyValue <= MaximumPrice).ToList();
+
+            return Json(QuerySet, JsonRequestBehavior.AllowGet);
+        }
     }
 }
