349e88627335f04ed6dc38b6065bce12e78edbfb
Itai Hay <3392524+itaihay@users.noreply.github.com>
2018-09-11 16:09:43 +0000

    Cleaned the project a bit

---
+++ Apartment-ML/ApartmentData.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Runtime.Api;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Transforms;
+using System;
+
+namespace Apartment_ML
+{
+    // STEP 1: Define your data structures
+
+    // IrisData is used to provide training data, and as 
+    // input for prediction operations
+    // - First 4 properties are inputs/features used to predict the label
+    // - Label is what you are predicting, and is only set when training
+    public class ApartmentData
+    {
+        [Column("0")]
+        public float ApartmentSize;
+
+        [Column("1")]
+        public float ApartmentPrice;
+
+        [Column("2")]
+        public float ApartmentFloorNumber;
+
+        [Column("4")]
+        [ColumnName("Label")]
+        public string Label;
+    }
+}
---
+++ Apartment-ML/ApartmentPrediction.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Runtime.Api;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Transforms;
+using System;
+
+namespace Apartment_ML
+{
+    // IrisPrediction is the result returned from prediction operations
+    public class ApartmentPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public string PredictedLabels;
+    }
+}
