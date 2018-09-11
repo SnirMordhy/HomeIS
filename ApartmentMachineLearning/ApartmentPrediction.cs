685b5e27be2319a7106e1000d6cf4d1313717341
Itai Hay <3392524+itaihay@users.noreply.github.com>
2018-09-11 21:19:00 +0000

    Created both projects

---
+++ ApartmentMachineLearning/ApartmentPrediction.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ML.Runtime.Api;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApartmentMachineLearning
+{
+    class ApartmentPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public string PredictedLabels;
+    }
+}
---
+++ ApartmentMachineLearningBridge/DoMLStuff.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApartmentMachineLearning;
+
+namespace ApartmentMachineLearningBridge
+{
+    class DoMLStuff
+    {
+        private PredictApartment pa;
+        public DoMLStuff()
+        {
+            pa = new PredictApartment();
+        }
+
+        public bool PredictApartmentSale(int size, int price, int floorNumber)
+        {
+            return pa.PredictApartmentSale(size, price, floorNumber);
+        }
+    }
+}
