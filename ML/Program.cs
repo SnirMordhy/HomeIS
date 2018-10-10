ab928da5abc18cc2f2c0481f29ef0c47123516ae
Itai Hay <3392524+itaihay@users.noreply.github.com>
2018-10-10 23:19:12 +0300

    Created project

---
+++ ML/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetOut(new StringWriter());
+            bool output = new PredictApartment().PredictApartmentSale(Int32.Parse(args[0]), Int32.Parse(args[1]), Int32.Parse(args[2]));
+
+            var standardOutput = new StreamWriter(Console.OpenStandardOutput());
+            standardOutput.AutoFlush = true;
+            Console.SetOut(standardOutput);
+
+            Console.WriteLine(output);
+            Console.ReadLine();
+        }
+    }
+}
