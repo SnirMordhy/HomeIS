de90ca8d233e48504c2d34b25f388c5621e085c5
Itai Hay <3392524+itaihay@users.noreply.github.com>
2018-10-14 00:29:41 +0300

    Fixed a bug where when you press a photo is would jump you back to the top of the screen

--- HomeIS/Scripts/site.js
+++ HomeIS/Scripts/site.js
@@ -133,7 +133,7 @@ function updateApartmentList(data) {
             $('#apartment-grid').append(
                 '<div class="col-sm-6 col-md-4">'
                 + '<div class="thumbnail">'
-                + '    <a href="#"><img data-apartmentid=' + index + ' src = "' + apartment.PhotoList[0] + '" class= "apartment-image" style = "height: 200px; width: 100%; display: block;"></a> '
+                + '    <a href="javascript:;"><img data-apartmentid=' + index + ' src = "' + apartment.PhotoList[0] + '" class= "apartment-image" style = "height: 200px; width: 100%; display: block;"></a> '
                 + '        <div class="caption ">'
                 + '            <h4 class="thumbnail-caption-header">' + apartment.Location.City + ', <small>' + apartment.Location.Neighborhood + '</small></h4>'
                 + '            <div class="row">'
